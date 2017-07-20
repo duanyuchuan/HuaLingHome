0f336872dc430f2273aeb5681488208c1e1cc4a1
duanyuchuan <18689478318@163.com>
2017-07-21 00:07:01 +0800

    2017.07.20主页布局

--- app/src/main/java/com/hualing/hualinghome/HomeActivity.java
+++ app/src/main/java/com/hualing/hualinghome/HomeActivity.java
@@ -1,13 +1,74 @@
 package com.hualing.hualinghome;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import com.hualing.hualinghome.base.BaseActivity;
+import com.hualing.hualinghome.fragment.FindFragment;
+import com.hualing.hualinghome.fragment.HomeFragment;
+import com.hualing.hualinghome.fragment.MyFragment;
+import com.hualing.hualinghome.fragment.ShardFragment;
+import com.hualing.hualinghome.fragment.ShopFragment;
+import com.hualing.hualinghome.interfaces.IBottomItemClickListener;
+import com.hualing.hualinghome.view.LayoutBottomBar;
 
-public class HomeActivity extends AppCompatActivity {
+/**
+ * Home页面
+ */
+public class HomeActivity extends BaseActivity implements IBottomItemClickListener {
+    private LayoutBottomBar mLayoutBottomBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //初始化视图
+        initView();
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        mLayoutBottomBar=findViewById(R.id.bottombar);
+        mLayoutBottomBar.setOnIBottomItemClickListener(this);
+    }
+
+    /**
+     * 点击底部栏
+     * @param view
+     */
+    @Override
+    public void onBottomItemClick(View view) {
+        switch (view.getId()){
+            case R.id.rl_home:
+                replaceContentByFragment(new HomeFragment());
+                break;
+            case R.id.rl_shard:
+                replaceContentByFragment(new ShardFragment());
+                break;
+            case R.id.rl_find:
+                replaceContentByFragment(new FindFragment());
+                break;
+            case R.id.rl_shop:
+                replaceContentByFragment(new ShopFragment());
+                break;
+            case R.id.rl_my:
+                replaceContentByFragment(new MyFragment());
+                break;
+        }
+    }
+    /**
+     * 切换Fragment
+     * @param fragment
+     */
+    private void replaceContentByFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+        fragmentTransaction.replace(R.id.fl_home_content,fragment);
+        fragmentTransaction.commit();
     }
 }
